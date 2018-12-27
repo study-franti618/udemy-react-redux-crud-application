2653a8050d53576944ae7cd438301abf66240b31
yoshizaki <yoshizaki@mobb-inc.jp>
2018-12-27 16:25:55 +0900

    型定義

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 // class App extends Component {
 //   render() {
@@ -11,19 +12,31 @@ import React from 'react';
 // }
 
 const App = () => {
+    const profiles = [
+        {name: "Taro", age: 10},
+        {name: "Jiro", age: 50},
+        {name: "Saro", age: 60},
+        {name: "1", age: 40},
+    ]
     return (
         <div>
-            <Cat/>
-            <Cat/>
-            <Cat/>
-            <Cat/>
+        {
+            profiles.map((profile,index) => {
+                return <User name={profile.name} age={profile.age} key={index}/>
+            })
+        }
         </div>
 
     )
 }
 
-const Cat = () => {
-    return <div>meow</div>
+const User = (props) => {
+    return <div>Hi I am {props.name}! and {props.age} yeas old</div>
+}
+
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
 }
 
 export default App;
