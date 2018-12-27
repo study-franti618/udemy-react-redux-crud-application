dc2a2ea61026eb1729724097bd4970041e299958
yoshizaki <yoshizaki@mobb-inc.jp>
2018-12-27 16:10:59 +0900

    meow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//         <React.Fragment>
+//         <input type="text" onChange={()=>{console.log('hiiiii')}} />
+//         </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hello world
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+        </div>
+
+    )
+}
+
+const Cat = () => {
+    return <div>meow</div>
 }
 
 export default App;
